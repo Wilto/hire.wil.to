84f5cfbf952e083929b95781383f57e9962b9d0b
Mat Marquis <mat@matmarquis.com>
2019-04-16 15:45:52 -0400

    Okay, getting the idea so far.

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,8 @@
 exports.handler = function(event, context, callback) {
+  const params = event.queryStringParameters;
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: `You sent the following: ${JSON.stringify(params)}`
   });
 };
