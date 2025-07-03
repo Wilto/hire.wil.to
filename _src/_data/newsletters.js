53e6793f5314f07006d4321df660c3fcb8b187c6
Mat Marquis <mat@matmarquis.com>
2025-07-03 09:22:55 -0400

    New newsletter.

--- _src/_data/newsletters.js
+++ _src/_data/newsletters.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const fetchCached = require( "@11ty/eleventy-fetch" );
 
 module.exports = async function() {
-  const api = "https://piccalil.li/api/emails-archive?stream=javascript-for-everyone";
+  const api = "https://piccalil.li/api/emails-archive?stream=javascript-for-everyone&quickcachebreak";
 
   /* This returns a promise */
   const response = await fetchCached( api, {
