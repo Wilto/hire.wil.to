05e7cc278fd94c7fa07b2d921971e55a9ddca104
Mat Marquis <mat@matmarquis.com>
2025-07-17 11:29:26 -0400

    Ah, websites.

--- _src/_data/newsletters.js
+++ _src/_data/newsletters.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const fetchCached = require( "@11ty/eleventy-fetch" );
 
 module.exports = async function() {
-  const api = "https://piccalil.li/api/emails-archive?stream=javascript-for-everyone";
+  const api = "https://piccalil.li/api/emails-archive?stream=javascript-for-everyone&rudebuster";
 
-  /* This returns a promise */
   const response = await fetchCached( api, {
     duration: "72h",
     type: "json"
